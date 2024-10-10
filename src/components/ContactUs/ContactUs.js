4d90b7a207be407b556f88720398616e78dc630b
Gabriel-Code-Commit-at-236865291904 <gabriel@kingdomsoftware.com.au>
2024-10-10 17:30:37 +1100

    Update subject

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -26,7 +26,7 @@ const ContactUs = () => {
           'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-          subject:"New contact from " + name,
+          subject: "New contact from " + name,
           message: "<html><body><p>" + name + "</p><p>" + email + "</p><p>" + message + "</p></body></html>",
           fromaddress: "sales@kingdomsoftware.com.au"
       })
