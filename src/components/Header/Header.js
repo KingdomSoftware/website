37ed50f5ee7a8cc558c5199bf5c92ac579d13353
Gabriel-Code-Commit-at-236865291904 <gabriel@kingdomsoftware.com.au>
2024-10-14 21:39:59 +1100

    Add hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,31 @@
 import React from 'react';
 import './Header.css'; // Import the CSS file for styling
 import logo from '../../images/KSLogo.png';
+import { Squash as Hamburger } from "hamburger-react";
 
 const Header = () => {
+    const [isOpen, setOpen] = useState(false);
+    
     return (
         <header>
             <div className="logo">
                 <img src={logo} alt="Kingdom Software Logo" />
             </div>
+            <div className="lg:hidden ">
+                <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+                {isOpen && (
+                    <div className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20">
+                        <ul className="grid gap-2">
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#testimonials">Testimonials</a></li>
+                            <li><a href="#about">About Us</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+            )}
+            </div>
             <nav>
-                <ul>
+                <ul className="hidden lg:flex lg:items-center gap-5 text-sm">
                     <li><a href="#services">Services</a></li>
                     <li><a href="#testimonials">Testimonials</a></li>
                     <li><a href="#about">About Us</a></li>
